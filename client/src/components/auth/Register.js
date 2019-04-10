0e906246422bd55f6370a13a68b694d5bd924095
andrewcooke89 <36635993+andrewcooke89@users.noreply.github.com>
2019-04-10 10:22:45 +0100

    added basic dashboard checks and added loading spinner

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class Register extends Component {
     constructor(){
@@ -12,9 +17,21 @@ class Register extends Component {
         }
     }
 
+    componentDidMount() {
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({errors: nextProps.errors})
+        }
+    };
+
     onChange = e => {
         this.setState({[e.target.name]: e.target.value});
-    }
+    };
 
     onSubmit = (e) => {
         e.preventDefault();
@@ -24,12 +41,17 @@ class Register extends Component {
             email: this.state.email,
             password: this.state.password,
             password2: this.state.password2
-        }
-        console.log(newUser)
-    } 
+        };
+
+        this.props.registerUser(newUser, this.props.history);
+       
+    };
 
 
   render() {
+
+    const { errors } = this.state;
+
     return (
         <div className="register">
             <div className="container">
@@ -41,58 +63,65 @@ class Register extends Component {
                     <p className="lead text-center">
                         Create your DevConnector account
                     </p>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <input 
-                                type="text" 
-                                className="form-control form-control-lg" placeholder="Name" 
-                                name="name"
-                                value={this.state.name}
-                                onChange={this.onChange}
-                             />
-                        </div>
-                        <div className="form-group">
-                            <input 
-                                type="email" 
-                                className="form-control form-control-lg" 
-                                placeholder="Email Address" 
-                                name="email" 
-                                value={this.state.email}
-                                onChange={this.onChange}
-                            />
-                        <small 
-                            className="form-text text-muted">
-                            This site uses Gravatar so if you want a profile image, use a Gravatar email
-                        </small>
-                        </div>
-                        <div className="form-group">
-                            <input 
-                                type="password" 
-                                className="form-control form-control-lg" placeholder="Password" 
-                                name="password" 
-                                onChange={this.onChange}
-                                value={this.state.password}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input 
-                            type="password" 
-                            className="form-control form-control-lg" placeholder="Confirm Password" name="password2" 
+                    <form noValidate onSubmit={this.onSubmit}>
+                        <TextFieldGroup 
+                            placeholder="Name"
+                            name="name"
+                            value={this.state.name}
                             onChange={this.onChange}
+                            error={errors.name}
+                        />
+                        
+                        <TextFieldGroup 
+                            placeholder="Email Address"
+                            name="email"
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.onChange}
+                            error={errors.email}
+                            info="This site uses Gravatar so if you want a profile image, use a Gravatar email"
+                        />
+                        
+                        <TextFieldGroup 
+                            placeholder="Password"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.onChange}
+                            error={errors.password}
+                        />
+
+                        <TextFieldGroup 
+                            placeholder="Confirm password"
+                            name="password2"
+                            type="password2"
                             value={this.state.password2}
-                            />
-                        </div>
-                            <input 
-                                type="submit" 
-                                className="btn btn-info btn-block mt-4" 
-                            />
-                        </form>
-                    </div>
+                            onChange={this.onChange}
+                            error={errors.password2}
+                        />
+                        
+                        <input 
+                            type="submit" 
+                            className="btn btn-info btn-block mt-4" 
+                        />
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
     )
   }
 }
 
-export default Register;
+Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
